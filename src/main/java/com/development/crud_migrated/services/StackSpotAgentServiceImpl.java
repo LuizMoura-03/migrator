52c6b0bae34adeb7386bf26d70e2f37a9fcd890d
LuizFernando <barbosa.luiz@zup.com.br>
2025-07-16 14:10:15 -0300

    :sparkles: feat: cria StackSpotAgentServiceImpl com método getAccessToken para autenticação via client credentials

---
+++ src/main/java/com/development/crud_migrated/services/StackSpotAgentServiceImpl.java
@@ -0,0 +1,43 @@
+package com.development.crud_migrated.services;
+
+import com.development.crud_migrated.config.StackSpotAgentConfig;
+import org.json.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+@Service
+public class StackSpotAgentServiceImpl implements StackSpotAgentService{
+
+    private StackSpotAgentConfig config;
+
+    @Autowired
+    public StackSpotAgentServiceImpl(StackSpotAgentConfig config) {
+        this.config = config;
+    }
+
+    @Override
+    public String getAccessToken() throws Exception {
+        String url = "https://idm.stackspot.com/" + config.getRealm() + "/oidc/oauth/token";
+        String body = "grant_type=client_credentials"
+                + "&client_id=" + config.getClientId()
+                + "&client_secret="
+                + config.getClientSecret();
+
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(url))
+                .header("Content-Type", "application/x-www-form-urlencoded")
+                .POST(HttpRequest.BodyPublishers.ofString(body))
+                .build();
+
+        HttpClient client = HttpClient.newHttpClient();
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        JSONObject json = new JSONObject(response.body());
+        return json.getString("access_token");
+    }
+
+}
