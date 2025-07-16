93e851e114abbea1e3c85ff00bd8923128f78f19
LuizFernando <barbosa.luiz@zup.com.br>
2025-07-16 14:31:30 -0300

    :sparkles: feat: adiciona AgentController com endpoint POST /agent/perguntar para consultar StackSpot Agent

---
+++ src/main/java/com/development/crud_migrated/controllers/AgentController.java
@@ -0,0 +1,30 @@
+package com.development.crud_migrated.controllers;
+
+import com.development.crud_migrated.services.StackSpotAgentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/agent")
+public class AgentController {
+
+    @Autowired
+    private StackSpotAgentService agentService;
+
+    @PostMapping("/perguntar")
+    public ResponseEntity<String> perguntarAoAgente(@RequestBody String pergunta) {
+        try {
+            String resposta = agentService.perguntarAoAgente(pergunta);
+            return ResponseEntity.ok(resposta);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body("Erro ao consultar agente: " + e.getMessage());
+        }
+    }
+
+}
