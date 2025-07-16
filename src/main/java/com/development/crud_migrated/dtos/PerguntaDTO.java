adda01b5d827f44c7b7d8e534c48367e8a424b45
LuizFernando <barbosa.luiz@zup.com.br>
2025-07-16 16:00:56 -0300

    :sparkles: feat: cria PerguntaDTO para receber JSON no endpoint /agent/perguntar

---
+++ src/main/java/com/development/crud_migrated/dtos/PerguntaDTO.java
@@ -0,0 +1,15 @@
+package com.development.crud_migrated.dtos;
+
+public class PerguntaDTO {
+
+    private String pergunta;
+
+    public String getPergunta() {
+        return pergunta;
+    }
+
+    public void setPergunta(String pergunta) {
+        this.pergunta = pergunta;
+    }
+
+}
